a4684953e9a3b82c54532328f704f3017cbcbeac
WilsonCheng <reborn2616am@gmail.com>
2022-09-26 15:58:26 +0800

    0926vue專案

---
+++ vueproject/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar></NavBar>
+  <ImgBar></ImgBar>
+  <SecondPart></SecondPart>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+import ImgBar from "./components/ImgBar.vue";
+import SecondPart from "./components/SecondPart.vue";
+export default {
+  components: {
+    NavBar,
+    ImgBar,
+    SecondPart,
+  },
+};
+</script>
+
+<style></style>
